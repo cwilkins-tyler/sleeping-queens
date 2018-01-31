d673b82ec5b43f246657bc03f57e98c5c85fed80
cwilkins <cwilkins@tylercapital.co.uk>
2018-01-31 10:48:14 +0000

    Added simple action card responses

--- src/queens.py
+++ src/queens.py
@@ -67,6 +67,7 @@ class Board:
                           'knight', 'dragon', 'jester']
         self.players = []
         self.player_turn = 0
+        self.cards_per_player = 5
         self.queen_cards = []
         self.playable_cards = []
         self.current_selection = []
@@ -130,15 +131,19 @@ class Board:
 
     def deal_initial_cards(self):
         shuffle(self.full_deck)
+        num_cards = len(self.full_deck)
         for card_index, target_position in enumerate(self.playable_card_positions):
-            self.move_card_to_destination(self.card_back, self.screen_center, target_position)
+            #self.move_card_to_destination(self.card_back, self.screen_center, target_position)
             self.playable_cards.append(target_position)
             target_player = self.players[card_index % len(self.players)]
             target_card = Card()
             target_card.card_type = self.full_deck[card_index]
+            self.full_deck.pop()
             target_card.pos_center(target_position[0], target_position[1])
             target_player.cards.append(target_card)
             self.initialise_board()
+
+        assert len(self.full_deck) == num_cards - len(self.players) * self.cards_per_player
 
     def initialise_queens(self):
         queen_vert_gap = int(float(self.screen.get_height()) / 9)
@@ -168,7 +173,7 @@ class Board:
             queen_name = self.queens[self.queen_cards.index(target_queen)]
             chosen_queen = os.path.join(self.resource_dir, 'queen-{}.jpg'.format(queen_name))
             queen_image = pygame.image.load(chosen_queen)
-            queen_image = pygame.transform.scale(queen_image, (60, 80))
+            queen_image = pygame.transform.scale(queen_image, (50, 70))
             target_queen.draw_card(self.screen, target_queen.center, queen_image, self.bg_colour)
             target_queen.select(self.screen)
         else:
@@ -176,6 +181,7 @@ class Board:
 
     def deselect_queens(self):
         for queen in self.queen_cards:
+            queen.selected = False
             queen.draw_card(self.screen, queen.center, self.queen_back_image, self.bg_colour)
 
     def move_card_to_destination(self, source_picture, source_coords, target_coords):
@@ -242,18 +248,69 @@ class Board:
         else:
             return False
 
+    @staticmethod
+    def card_is_number_card(card):
+        if re.search('^\d+$', card.card_type):
+            return True
+        else:
+            return False
+
     def valid_move(self):
         if len(self.current_selection) == 1:
             # any single card can be played
             return True
         else:
-            # multiple cards must all be numbers
+            # multiple cards must all be numbers, and either all the same or a sum
             all_numbers = True
+            valid_numbers = set()
             for card in self.current_selection:
-                if not re.search('^\d+$', card.card_type):
+                if self.card_is_number_card(card):
                     all_numbers = False
+                    break
 
-            return all_numbers
+                valid_numbers.add(card.card_type)
+
+            return all_numbers and len(valid_numbers) == 1
+
+    def action_card_selected(self):
+        for card in self.current_selection:
+            if not self.card_is_number_card(card):
+                return True
+
+        return False
+
+    def perform_action(self):
+        action_card = self.current_selection[0]
+
+        if action_card.card_type == 'king':
+            print('Playing king')
+        elif action_card.card_type == 'jester':
+            print('jester')
+        elif action_card.card_type == 'knight':
+            print('knight')
+        elif action_card.card_type == 'dragon':
+            print('dragon')
+        elif action_card.card_type == 'potion':
+            print('potion')
+        elif action_card.card_type == 'wand':
+            print('wand')
+
+    def replace_cards(self):
+        current_player = self.players[self.player_turn]
+        num_cards = len(self.full_deck)
+        for i in range(len(self.current_selection)):
+            old_card = self.current_selection[i]
+            target_card = Card()
+            target_card.card_type = self.full_deck[-1]
+            print('Replacing {} with {}'.format(old_card.card_type, target_card.card_type))
+            self.full_deck.pop()
+            target_card.center = old_card.center
+            current_player.cards.remove(old_card)
+            current_player.cards.append(target_card)
+
+        assert len(current_player.cards) == self.cards_per_player, len(current_player.cards)
+        assert len(self.full_deck) == num_cards - len(self.current_selection)
+        self.current_selection = []
 
     def do_player_turn(self):
         self.center_stack = self.draw_center_card()
@@ -268,12 +325,19 @@ class Board:
                     print('Player Card selected')
                 elif self.is_center_card_selected():
                     if self.valid_move():
-                        print('Moving cards')
+                        if self.action_card_selected():
+                            self.perform_action()
+                        print('Replacing cards')
+                        self.replace_cards()
                     else:
                         print('Not a valid move')
+                        for card in self.current_selection:
+                            print(card.card_type)
 
                     print('Ending turn: {}'.format(self.player_turn))
                     self.hide_player_cards()
+                    self.deselect_queens()
+                    self.current_selection = []
                     self.player_turn += 1
                     self.player_turn %= len(self.player_names)
                 else:
